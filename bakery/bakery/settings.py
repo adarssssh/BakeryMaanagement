a55fa12971340dca64a3d5033103e60ce04a1d14
Adarsh Tiwari <76429293+adarssssh@users.noreply.github.com>
2021-11-08 11:05:41 +0530

    Update settings.py

--- bakery/bakery/settings.py
+++ bakery/bakery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ud6pem6x(c$fdx^)%#+n@%&_3ta@3c%k9_6fw8cz!^d@($g_e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
